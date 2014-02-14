eb9318aeddb9a00f8534b037b518557a37ace22e
Arman Jakupovic <xclusiv8@gmail.com>
2014-02-14 17:38:20 +0100

    Håller på med musikbiten. Lagt till fyra låtar.

---
+++ SudokuMain/SudokuMain/MusicHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+using WMPLib;
+using System.Threading;
+
+namespace SudokuMain
+{
+    class MusicHandler
+    {
+        Random _newRnd;
+        WindowsMediaPlayer _myPlayer;
+        Thread _myThread;
+        public MusicHandler()
+        {
+            _myPlayer = new WindowsMediaPlayer();
+            _newRnd = new Random();
+        }
+
+        public void PlayMusic()
+        {
+            int x = _newRnd.Next(1, 5);
+            string path = String.Format("Music\\{0}.wav", x.ToString());
+            _myPlayer.URL = path;
+            _myThread = new Thread(_myPlayer.controls.play);
+            _myThread.Start();
+        }
+    }
+}
+
