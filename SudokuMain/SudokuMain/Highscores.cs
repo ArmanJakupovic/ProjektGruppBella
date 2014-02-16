e4c817f2c0ce07c6bf994b4dd6c3a7ebd83bfadd
Fredrik Johanzon <fredrik.johanzon@gmail.com>
2014-02-16 13:28:12 +0100

    Fixat bugg i higscore. Det sparas fortfarande som int men visas som tid.

--- SudokuMain/SudokuMain/Highscores.cs
+++ SudokuMain/SudokuMain/Highscores.cs
@@ -12,8 +12,8 @@ namespace SudokuMain
         private List<string> _name = new List<string>();//lista med namn
         private List<int> _score = new List<int>();//lista med poäng
         public void LoadScore(string value, int points) { _name.Add(value); _score.Add(points); }//Ladda in till listorna
-        //public string GetScore(int index) { return _name[index] + "\t" + _score[index].ToString(); }//Returnerar en sträng med namn och poäng
-        public string GetScore(int index) { return _name[index] + "\t     " + (DateTime.Today + new TimeSpan(0, 0, _score[index])).ToString("H:mm:ss"); }//Returnerar en sträng med namn och poäng
+        public string GetScore(int index) { return _name[index] + "\t" + _score[index].ToString(); }//Returnerar en sträng med namn och poäng
+        public string GetTime(int index) { return _name[index] + "\t     " + (DateTime.Today + new TimeSpan(0, 0, _score[index])).ToString("H:mm:ss"); }//Returnerar en sträng med namn och poäng
         public void InsertScore(string name, int points, int index) { _name.Insert(index, name); _score.Insert(index, points); }//Placerar en ny person på listan
         public void RemoveLast(int index) { _name.RemoveAt(index); _score.RemoveAt(index); }//Tar bort en person från listan på indexets plats. Ämnat för sista personen
         public List<int> GetPoints() { return _score; } //Returnerar en lista med poäng
@@ -108,7 +108,7 @@ namespace SudokuMain
             string strHighscore = "";
             for (int i = 0; i < _numberOfNames; i++)
             {
-                strHighscore += (i + 1).ToString() + ". " + _highscoreList[diff,lvl].GetScore(i) + "\n";//namn och tab
+                strHighscore += (i + 1).ToString() + ". " + _highscoreList[diff,lvl].GetTime(i) + "\n";//namn och tab
             }
             return strHighscore;
         }
